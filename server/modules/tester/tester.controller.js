a083389e0250ce53c3f7b180dc729160dc33bc78
Jason humphrey <jason.humphrey@live.com>
2015-12-31 16:17:35 -0500

    took out the examples

--- server/modules/tester/tester.controller.js
+++
@@ -1,5 +0,0 @@
-exports.tester = function (req, res) {
-  return res.status(200).send({
-    query: req.queryParameters
-  })
-}
--- server/modules/tester/tester.routes.js
+++
@@ -1,6 +0,0 @@
-var express = require('express')
-var tester = require('./tester.controller.js')
-var app = express()
-
-app.get('/tester/', tester.tester)
-module.exports = app
