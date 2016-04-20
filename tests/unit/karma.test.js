b6faeac4093e393fb29cdec2e45eed8b9b9d4831
Javier Pena <javier@javierdev.com>
2016-04-20 12:24:25 -0500

    #35 Added chai to app and pulled in dependency files for karma conf

--- tests/unit/karma.test.js
+++ tests/unit/karma.test.js
@@ -1,19 +1,29 @@
 // Karma configuration
 
 module.exports = function (config) {
+  var path = require('path')
+  var _ = require('lodash')
+  var dependencies = require('../../configs/settings.js').get().assets.js
+  var dependencyFiles = _.map(dependencies, function (dependency) {
+    return path.resolve('./client/' + dependency).toString()
+  })
+  var clientPath = '../../client'
+  var srcFiles = [
+    clientPath + '/modules/**/*.module.js',
+    clientPath + '/modules/**/*.js'
+  ]
+  var files = dependencyFiles.reverse().concat(srcFiles)
+
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: './',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'chai'],
 
     // list of files / patterns to load in the browser
-    files: [
-      '../../client/modules/**/*.spec.js',
-      './../client/modules/**/*.spec.js'
-    ],
+    files: files,
 
     // list of files to exclude
     exclude: [],
@@ -55,6 +65,7 @@ module.exports = function (config) {
 
     plugins: [
       'karma-mocha',
+      'karma-chai',
       'karma-chrome-launcher'
     ]
   })
