8e182af6b5f352e016f982d99a099564618d2e99
Jason Humphrey <Humphrey.jason32@gmail.com>
2016-05-22 18:29:26 -0500

    took out some comments

--- client/modules/core/util.provider.js
+++ client/modules/core/util.provider.js
@@ -104,9 +104,6 @@
     }
   }
 
-  /**
-   * Must configure the exception handling
-   */
   function exceptionHandlerProvider () {
     this.config = {
       appErrorPrefix: undefined
@@ -122,41 +119,17 @@
   }
 
   config.$inject = ['$provide']
-
-  /**
-   * Configure by setting an optional string value for appErrorPrefix.
-   * Accessible via config.appErrorPrefix (via config value).
-   * @param  {Object} $provide
-   */
-  /* @ngInject */
   function config ($provide) {
     $provide.decorator('$exceptionHandler', extendExceptionHandler)
   }
 
   extendExceptionHandler.$inject = ['$delegate', 'exceptionHandler', 'logger']
-
-  /**
-   * Extend the $exceptionHandler service to also display a toast.
-   * @param  {Object} $delegate
-   * @param  {Object} exceptionHandler
-   * @param  {Object} logger
-   * @return {Function} the decorated $exceptionHandler service
-   */
   function extendExceptionHandler ($delegate, exceptionHandler, logger) {
     return function (exception, cause) {
       var appErrorPrefix = exceptionHandler.config.appErrorPrefix || ''
       var errorData = {exception: exception, cause: cause}
       exception.message = appErrorPrefix + exception.message
       $delegate(exception, cause)
-      /**
-       * Could add the error to a service's collection,
-       * add errors to $rootScope, log errors to remote web server,
-       * or log locally. Or throw hard. It is entirely up to you.
-       * throw exception
-       *
-       * @example
-       *     throw { message: 'error message we added' }
-       */
       logger.error(exception.message, errorData)
     }
   }
