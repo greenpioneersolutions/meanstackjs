081f6ea4f6f22b298960b5969ccd4facad13433f
Jason Humphrey <Humphrey.jason32@gmail.com>
2016-11-05 01:07:51 -0500

    #137 added the models to all routers

--- server/modules/system/system.routes.js
+++ server/modules/system/system.routes.js
@@ -2,7 +2,7 @@ var system = require('./system.controller.js')
 var pug = require('pug')
 var path = require('path')
 
-module.exports = function (app, auth, mail, settings) {
+module.exports = function (app, auth, mail, settings, models) {
   app.get('/api/testing/', system.testing(mail, settings))
   app.get('/api/settings/', function (req, res) {
     res.send(pug.renderFile(path.join(__dirname, 'setting.view.pug'), {settings: settings}))
