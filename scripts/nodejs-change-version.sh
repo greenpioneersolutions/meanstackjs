fae6f036643fff87ecb48b5d6a6d1488cfa4a713
Ionică Bizău <bizauionica@gmail.com>
2016-12-30 09:46:05 +0200

    Remove commented lines

--- scripts/nodejs-change-version.sh
+++ scripts/nodejs-change-version.sh
@@ -2,13 +2,6 @@
 # if you need more help installing node - https://gist.github.com/isaacs/579814
 #
 
-#if [ "$(uname)" == "Darwin" ]; then
-#	echo "You're using macOS."
-#elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-#	echo "You're using Linux."
-#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-#fi
-
 [ $(node -p process.platform) == "win32" ] && echo "Please Update Node.js from https://nodejs.org and install it." && exit 1
 
 export NVM_DIR="$HOME/.nvm"
