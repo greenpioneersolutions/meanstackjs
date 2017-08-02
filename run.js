b6702b8b61d3410bd6292f1b22e6613f4deed14d
Jason Humphrey <humphrey.jason32@gmail.com>
2017-08-01 20:16:03 -0500

    #275 moved the disconnect to db.js

--- run.js
+++ run.js
@@ -31,7 +31,13 @@ process.on('unhandledRejection', function (reason) {
 })
 
 process.on('uncaughtException', function (err) {
-  require('mongoose').disconnect()
+  try{
+    require('./server/db.js').disconnect(function(){
+      console.log('Disconnected Database')
+    })
+  }catch(disconnectError){
+    console.log('Error Trying to disconnect from the DB',disconnectError)
+  }
   debug('System Error uncaughtException:' + err)
   console.error('[UNCAUGHT EXCEPTION] - ', err.message)
   console.log()
